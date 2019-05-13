831f788112a31ac547827a7a476653bafc43d9ed
Gerardo Castillo <dev.gerardocd@gmail.com>
2019-05-13 10:03:19 -0500

    FirstCommit

---
+++ app.js
@@ -0,0 +1,20 @@
+var express = require('express');
+var app = express();
+
+app.get('/data', function(req, res) {
+    let salida = {
+        nombre: "ferando",
+        edad: 15,
+        url: req.url
+    }
+    res.send(salida)
+
+    res.send('Hello World');
+})
+
+var server = app.listen(process.env.PORT || 8081, function() {
+    var host = server.address().address
+    var port = server.address().port
+
+    console.log("Example app listening at http://%s:%s", host, port)
+})
